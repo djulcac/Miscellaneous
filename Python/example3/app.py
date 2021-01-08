a040e440d5678bde4513240ae65392b280f9e4c6
Danny Julca <djulcac@uni.pe>
2021-01-08 02:00:50 -0500

    python 3

--- Python/example3/app.py
+++ Python/example3/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, copy_current_request_context
+from flask import Flask, render_template, session, copy_current_request_context, request
 from flask_socketio import SocketIO, emit, disconnect
 from threading import Lock
 
@@ -6,42 +6,41 @@ from threading import Lock
 async_mode = None
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socket_ = SocketIO(app, async_mode=async_mode)
+socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
 
+clients = 0
 
 @app.route('/')
 def index():
-    return render_template('index.html', async_mode=socket_.async_mode)
+    return render_template('index.html', async_mode=socketio.async_mode)
 
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    global clients
+    clients += 1
+    print('client connected', clients, request.sid)
 
-@socket_.on('my_event', namespace='/test')
+@socketio.on('my_event', namespace='/test')
 def test_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']})
+    emit('my_response', {'data': message['data']})
 
 
-@socket_.on('my_broadcast_event', namespace='/test')
+@socketio.on('my_broadcast_event', namespace='/test')
 def test_broadcast_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         broadcast=True)
+    emit('my_response', {'data': message['data']}, broadcast=True)
 
 
-@socket_.on('disconnect_request', namespace='/test')
+@socketio.on('disconnect_request', namespace='/test')
 def disconnect_request():
     @copy_current_request_context
     def can_disconnect():
+        global clients
+        clients -= 1
         disconnect()
 
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']},
-         callback=can_disconnect)
-
+    emit('my_response', {'data': 'Disconnected!'}, callback=can_disconnect)
 
 if __name__ == '__main__':
-    socket_.run(app, debug=True)
+    socketio.run(app, debug=True)
